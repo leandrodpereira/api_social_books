5de812029b81a321707f9b7091724c42ce843c9a
Leandro <leandro.pereira@fepi.br>
2022-10-25 15:30:56 -0300

    Salvando o recurso Livro a partir de um POST

---
+++ Aula04-CRUD/src/main/java/br/fepi/socialbooks/resoucers/LivrosResources.java
@@ -0,0 +1,34 @@
+package br.fepi.socialbooks.resoucers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.LivrosRepository;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivrosResources {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro> listar() {
+		
+		return livrosRepository.findAll();
+				
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		
+		livrosRepository.save(livro);
+	}
+	
+}
