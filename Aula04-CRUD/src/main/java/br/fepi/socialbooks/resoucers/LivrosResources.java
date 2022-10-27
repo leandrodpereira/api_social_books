85e0a42b79c7dcf6e62d7323ddeb00626b5cb3ee
Leandro <leandro.pereira@fepi.br>
2022-10-27 16:51:04 -0300

    Buscando um livro com o uso da anotação @PathVariable

--- Aula04-CRUD/src/main/java/br/fepi/socialbooks/resoucers/LivrosResources.java
+++ Aula04-CRUD/src/main/java/br/fepi/socialbooks/resoucers/LivrosResources.java
@@ -3,6 +3,7 @@ package br.fepi.socialbooks.resoucers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +30,13 @@ public class LivrosResources {
 	public void salvar(@RequestBody Livro livro) {
 		
 		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro buscar (@PathVariable("id") Long id) {
+		
+		return livrosRepository.findById(id).orElse(null);		
+		
 	}
 	
 }
